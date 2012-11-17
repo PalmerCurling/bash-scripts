0edd6e226702e2a4f9de7b173b70ee0950ccfe19
palmercurling <palmercurling@android.net>
2012-11-16 20:13:58 -0500

    first push of scripts

---
+++ lightcontrol.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+cd ~/Downloads/pyalienfx
+gksudo python ./pyAlienFX.py
---
+++ meadownas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo mount -t cifs -o user=eric,password=Thrashers1999 //192.168.1.3/public ./network
+
