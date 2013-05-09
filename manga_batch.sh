137f348279279bfa5e402fb6be477131e2b8b034
Eric Meadow <palmercurling@android.net>
2013-05-08 23:00:04 -0400

    added manga batch

---
+++ manga_batch.sh
@@ -0,0 +1,18 @@
+#use this to batch rename a folder of pdfs to cbz for use in comic readers
+IFS="\n"
+ls ./*.pdf > pdf
+cat pdf | while read a;
+do
+	chapter=${a}
+	convert $chapter "$chapter.jpg"
+	tar -czvf ./$chapter.cbz *.jpg
+	rm *.jpg
+done
+
+for file in ./*.pdf.cbz
+do
+	mv file file.cbz
+done
+
+rm pdf
+unset IFS;
